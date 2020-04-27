5a574aad5da3458952374bcf38b2e5dcf38fa40b
5f1839842f2fb <57242175+5f1839842f2fb@users.noreply.github.com>
2020-04-27 09:03:09 -0400

    mvp?

--- auth-friends-client/src/App.js
+++ auth-friends-client/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+
+import Login from './components/Login'
+import FriendsList from './components/FriendsList'
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false) //only exists to force rerender when logging in successfully
+  /* useEffect(() => {
+    localStorage.clear()
+  },[]) */
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        {/* <Switch>
+          <Route exact path="/" component={Login}/>
+        </Switch> */}
+        {!localStorage.getItem('token')
+          ? <Login setLoggedIn={setLoggedIn}/> 
+          : <FriendsList/>}
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ auth-friends-client/src/components/Login.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import axios from 'axios'
+
+const Login = props => {
+  const [login, setLogin] = useState({ username: '', password: '' })
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    setIsLoading(true)
+
+    axios.post("http://localhost:5000/api/login", login)
+    .then(response => {
+      setIsLoading(false)
+      console.log(response)
+      localStorage.setItem('token', response.data.payload)
+      props.setLoggedIn(true)
+    })
+    .catch(error => {
+      setIsLoading(false)
+      console.log(error)
+    })
+  }
+
+  return (
+    <div>
+      {!isLoading 
+      ? <form onSubmit={handleSubmit}>
+          <input placeholder="Username" onChange={event => setLogin({...login, username: event.target.value})}/>
+          <input placeholder="Password" onChange={event => setLogin({...login, password: event.target.value})}/>
+          <button>Submit</button>
+        </form>
+      : <h1>loading</h1>}
+    </div>
+  )
+}
+
+export default Login
